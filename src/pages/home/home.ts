62d3e313afa33837cd7e7962464fa2ab6be7a4ee
Matthieu Drula <matthieu.drula.hs@gmail.com>
2017-06-30 08:09:05 -0400

    all good

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GoogleAnalytics } from '@ionic-native/google-analytics';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  hasAnswered = false;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public ga: GoogleAnalytics) {
+    this.ga.startTrackerWithId('UA-101858534-1')
+      .then(() => {
+        console.log('Google analytics is ready now');
+        this.ga.trackView('home');
+      })
+      .catch(e => console.log('Error starting GoogleAnalytics', e));
   }
 
+  answer(number) {
+    this.ga.trackEvent('form', 'vote', number)
+    .then(() => {
+      this.hasAnswered = true;
+    });
+  }
 }
